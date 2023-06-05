269bf5ba31c60c811107d56a2fc89ab56a2af032
Fawaz Al Khreisha <alkhreisha02@gmail.com>
2023-06-05 22:01:15 +1000

    Delete Advanced Pool Game directory

--- Advanced Pool Game/src/main/java/PoolGame/Observer/Observer.java
+++
@@ -1,8 +0,0 @@
-package PoolGame.Observer;
-/** Observer updating interface for objects to be notified of changes
- */
-public interface Observer {
-    /** Updates to keep the state consistent*/
-    public void update();
-    public int getScore();
-}
