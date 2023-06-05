754ef780b2ba380ed817ab67bb06100b1b1d4171
Fawaz Al Khreisha <alkhreisha02@gmail.com>
2023-06-05 22:01:03 +1000

    Delete Assignment2Final directory

--- Assignment2Final/src/main/java/org/example/BallStrategy.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-import java.util.List;
-
-public interface BallStrategy {
-    List<Components> ballIn(boolean in, List<Components> balls, List<List<Double>> pockets, GameWindow w);
-}
